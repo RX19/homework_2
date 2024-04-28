fc4ac92a73436fd3e0284546787214bbca5ed9ab
TRX <rusbelx@hotmail.com>
2024-04-28 08:22:24 -0600

    CRUD finished in exp model

--- exp/models/exp.model.php
+++ exp/models/exp.model.php
@@ -31,7 +31,6 @@ class Exp extends Conn{
         try{
             $sql = "INSERT INTO expediente (idexp, dnte, dndo, ofen, desch, fden, fsuc) VALUES (:idexp, :dnte, :dndo, :ofen, :desch, :fden, :fsuc)";
             $stmt = Conn::getConn()->prepare($sql);
-            $stmt->bindValue(":idexp", $data['idexp']);
             $stmt->bindValue(":dnte", $data['dnte']);
             $stmt->bindValue(":dndo", $data['dndo']);
             $stmt->bindValue(":ofen", $data['ofen']);
@@ -44,6 +43,24 @@ class Exp extends Conn{
             $e->getMessage();
         }
     }
+
+    public static function editData($data){
+        try{
+            $sql = "UPDATE expediente SET dnte = :dnte, dndo = :dndo, ofen = :ofen, desch = :desch, fden = :fden, fsuc = :fsuc WHERE idexp = :idexp";
+            $stmt = Conn::getConn()->prepare($sql);
+            $stmt->bindValue(":dnte", $data['dnte']);
+            $stmt->bindValue(":dndo", $data['dndo']);
+            $stmt->bindValue(":ofen", $data['ofen']);
+            $stmt->bindValue(":desch", $data['desch']);
+            $stmt->bindValue(":fden", $data['fden']);
+            $stmt->bindValue(":fsuc", $data['fsuc']);
+            $stmt->execute();
+            return true;
+        } catch(PDOException $e){
+            $e->getMessage();
+        }
+    }
+
 
     public static function delData($idexp){
         try{
